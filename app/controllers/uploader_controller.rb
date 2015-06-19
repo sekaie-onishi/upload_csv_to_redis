4f6b381023adac0d13a9d1b9c3061e05151e0586
onishi <k.onishi@sekaie.co.jp>
2015-06-19 15:34:11 +0900

    test

---
+++ app/controllers/uploader_controller.rb
@@ -0,0 +1,9 @@
+class UploaderController < ApplicationController
+  def index
+  end
+  def import
+    importRedis = ImportRedis.new
+    @text = importRedis.import(params[:file])
+    #redirect_to root_url, notice: "CSV imported."
+  end
+end
---
+++ app/models/import_redis.rb
@@ -0,0 +1,32 @@
+require 'json'
+class ImportRedis
+  include ActiveModel::Model
+
+  def initialize
+    @redis_value_keys = ["page_name", "title", "h1", "description", "keyword",
+                         "pan1", "pan2", "pan3", "pan4", "pan5"]
+  end
+
+  def import(file)
+    @text = "keys:"
+    @redis = Redis.new(:host => Settings.redis_setting.host, :port => Settings.redis_setting.port)
+    CSV.foreach(file.path, {headers: true, quote_char: "'"}) do |row|
+      key = "#{Settings.redis_setting.host_prefix}#{Settings.redis_common.key_separator}
+             #{Settings.redis_common.data_type_key}#{Settings.redis_common.key_separator}
+             #{row['theme']}#{row['controller']}#{row['action']}"
+      value = create_value(row)
+      @redis.set(key, value);
+      #@text += "#{key}: #{JSON.generate(value)}"
+    end
+    return @text
+  end
+
+  def create_value(row)
+    value = Hash.new()
+    @redis_value_keys.each{|key|
+      value[key] = row[key]
+    }
+    return value
+  end
+
+end
